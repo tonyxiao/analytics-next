6e5207349832480acda001ae32bb7e139584e010
Tony Xiao <tonyx.ca@gmail.com>
2019-04-04 05:22:48 -0700

    Fix jest config

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
     '^.+\\.tsx?$': 'ts-jest',
   },
   transformIgnorePatterns: ['<rootDir>/node_modules/'],
-  setupTestFrameworkScriptFile: './scripts/setup-test-framework.ts',
+  setupFilesAfterEnv: './scripts/setup-test-framework.ts',
   testRegex: '(\\.(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   modulePathIgnorePatterns: ['__mocks__/.+.jsx?$', '__mocks__/.+.tsx?$'],
